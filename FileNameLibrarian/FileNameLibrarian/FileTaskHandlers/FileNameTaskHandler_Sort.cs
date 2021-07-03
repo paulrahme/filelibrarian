cc4eaee47ea76900ff97a4cd843d2fdbd649bbad
Paul Rahme <paulrahme@yahoo.com>
2021-07-03 18:31:27 +0100

    Interim: First command (sort) working, sorting by filename. About to refactor into command-driven.

--- FileNameLibrarian/FileNameLibrarian/FileTaskHandlers/FileNameTaskHandler_Sort.cs
+++ FileNameLibrarian/FileNameLibrarian/FileTaskHandlers/FileNameTaskHandler_Sort.cs
@@ -1,4 +1,8 @@
-﻿namespace FileNameLibrarian
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FileNameLibrarian
 {
 	public class FileTaskHandler_Sort : FileTaskHandler
 	{
@@ -7,7 +11,29 @@
 
 		public override int ProcessTask(string baseDir, string pattern)
 		{
+			List<DirectoryInfo> allFiles = GetAllFiles(baseDir, pattern);
+
+			string debugStr = "BEFORE SORT:";
+			for (int i = 0; i < allFiles.Count; ++i)
+				debugStr += $"\n{allFiles[i].FullName}";
+
+			allFiles.Sort(new SortComparer());
+
+			debugStr += "\n\nAFTER SORT:";
+			for (int i = 0; i < allFiles.Count; ++i)
+				debugStr += $"\n{allFiles[i].FullName}";
+
+			Console.WriteLine(debugStr);
+
 			return 0;
+		}
+
+		private class SortComparer : IComparer<DirectoryInfo>
+		{
+			public int Compare(DirectoryInfo x, DirectoryInfo y)
+			{
+				return string.Compare(x.Name, y.Name);
+			}
 		}
 	}
 }
